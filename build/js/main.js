15cfde2ec835fed3f5dc85bb55944e86e843e316
zinchandrey <zinchandrey@gmail.com>
2020-02-28 00:13:03 +0300

    Добавляет блок переключение цветов в карточке товара, PP сайдбара, открытие и закрытие меню при клике, добавляет разметку меню на все страницы

--- build/js/main.js
+++ build/js/main.js
@@ -1,11 +1,15 @@
 'use strict';
+
 // сайдбар с фильтрами на странице каталога
 var sidebar = document.querySelector('.sidebar');
 var filters = document.querySelector('.toolbar__item-link--mob');
 var filterCloseButton = document.querySelector('.sidebar__close-button');
+var catalog = document.querySelector('.catalog');
 
-// открытие / закрытие сайдбара
+// открытие / закрытие сайдбара с фильтрами
+// убирает сайдбар из потока и закрывает его при включ js
 sidebar.classList.add('sidebar--closed');
+catalog.classList.remove('catalog--no-js'); // расскоментировать соответствующий css
 
 filters.addEventListener('click', function () {
   sidebar.classList.toggle('sidebar--closed');
@@ -23,6 +27,39 @@ for (var i = 0; i < filterName.length; i++) {
     event.preventDefault();
     event.target.classList.toggle('sidebar__legend-button--closed');
     event.target.parentElement.nextElementSibling.classList.toggle('closed');
+  });
+}
+
+// открытие / закрытие меню в моб версии
+var nav = document.querySelector('.nav');
+var menuCloseButton = document.querySelector('.nav__button-close');
+var menuOpenButton = document.querySelector('.page-header__menu-button');
+var menuMob = document.querySelector('.nav');
+
+// закрывает меню и убираем его из потока при работающем js
+nav.classList.remove('nav--no-js'); // необходимо раскомментировать css код
+menuMob.classList.add('closed');
+
+menuOpenButton.addEventListener('click', function () {
+  menuMob.classList.remove('closed');
+});
+
+menuCloseButton.addEventListener('click', function () {
+  menuMob.classList.add('closed');
+});
+
+
+// переключение цветов в карточке товара
+
+var colorButton = document.querySelectorAll('.product__color-button');
+
+for (i = 0; i < colorButton.length; i++) {
+  colorButton[i].addEventListener('click', function (event) {
+    event.preventDefault();
+    for (i = 0; i < colorButton.length; i++) {
+      colorButton[i].classList.remove('product__color-button--active');
+    }
+    event.target.classList.toggle('product__color-button--active');
   });
 }
 
